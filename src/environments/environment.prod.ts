049bafa98d520d50d1ff913a8500e4c7160e2fae
phuongThao460 <blackcompany1998@gmail.com>
2022-05-09 15:59:08 +0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,24 @@
+export const environment1 = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXVTjTuBUXf-UWlpKfKHjomfPezutmPwI",
+    authDomain: "cas-fee-shop.firebaseapp.com",
+    databaseURL: "https://cas-fee-shop.firebaseio.com",
+    projectId: "cas-fee-shop",
+    storageBucket: "cas-fee-shop.appspot.com",
+    messagingSenderId: "323643286137",
+  },
+};
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDIUHoFHNlaswHGrS_5dpLaoGf8761Fr3Q",
+    authDomain: "shop-angular-b885d.firebaseapp.com",
+    databaseURL: "https://shop-angular-b885d-default-rtdb.firebaseio.com",
+    projectId: "shop-angular-b885d",
+    storageBucket: "shop-angular-b885d.appspot.com",
+    messagingSenderId: "572616641433",
+    appId: "1:572616641433:web:904c68b3964cb181b2e108",
+  },
+};
